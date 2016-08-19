6af3f436c63e1171bd3d0cbc61142c3f24f5c9ca
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-08-18 17:07:13 -0700

    redux actions added

--- store.js
+++ store.js
@@ -12,7 +12,37 @@ var defaultState = {
   }
 };
 
-// add actions
+// actions
+function addTodo(message) {
+  return {
+    type: 'ADD_TODO',
+    message: message,
+    completed: false
+  };
+}
+
+function completeTodo(index) {
+  return {
+    type: 'COMPLETE_TODO',
+    index: index
+  };
+}
+
+function deleteTodo(index) {
+  return {
+    type: 'DELETE_TODO',
+    index: index
+  };
+}
+
+function clearTodo() {
+  return {
+    type: 'CLEAR_TODO'
+  };
+}
+
+
+// REDUCER 
 function todoApp(state, action) {
   switch (action.type) {
     case 'ADD_TODO':
